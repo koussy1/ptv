48f7de2b1a22fdad9b4c5819d7b5f8c2adde8c63
koussy1 <koussy@gmail.com>
2026-05-04 22:33:49 +0100

    Delete manifest.js

--- manifest.js
+++
@@ -1,9 +0,0 @@
-{
-  "name": "K-Player Pro",
-  "short_name": "KPlayer",
-  "start_url": "index.html",
-  "display": "standalone",
-  "background_color": "#0c0e1e",
-  "theme_color": "#00e676",
-  "orientation": "landscape"
-}
